98ac9fcdbfc435fa78e3879d8bd3e41b92e83adb
Hunobas <po127992@gmail.com>
2021-10-09 17:37:24 +0900

    add Eigen aliasing tutorial

---
+++ Eigen3_Tutorial/Eigen3_Tutorial.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <Eigen/Dense>
+
+using namespace Eigen;
+using namespace std;
+
+int main()
+{
+	MatrixXf mat(3, 3);
+	mat << 1.5, 2, 3, 4, 5, 6, 7, 8, 9;
+	cout << "Here is the matrix mat:\n" << mat << endl;
+
+	// This assignment shows the aliasing problem
+
+	cout << endl << mat.bottomRightCorner(2, 2) << endl << mat.topLeftCorner(2, 2) << endl << endl;
+	mat.bottomRightCorner(2, 2) = mat.topLeftCorner(2, 2);
+
+	
+	cout << "After the assignment, mat = \n" << mat << endl;
+}
